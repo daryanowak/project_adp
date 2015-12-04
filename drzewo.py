b1bda498efb9614512bb0b0e03810361925a4982
Adao <adao@debianDell.Adao>
2015-12-04 16:28:17 +0100

    Poprawione po tescie do dziewczyn

--- drzewo.py
+++ drzewo.py
@@ -13,11 +13,13 @@ class Node():
         """Dodawanie calych macierzy z wartosciami do naszego drzewa,
         macierz ktora wprowadzamy musi miec na pozycji [n][-1] poprana predykcje, gdzie n jest liczba od 0 do len(m).
         Sposob podzialu na podstawie indeksu Giniego"""
-        if len(m) == 0:
+        if len(m) == 0 :
             return
-        elif self.check_last(m): #sprawdzenie czy wszystkie klasyfikacje sa poprawne
-             self.classification = (1, m[0][-1] )
-             print self.classification
+        if type(m[0]) != list:
+            m= [m]
+        if len(m)== 1 or self.check_last(m): #sprawdzenie czy wszystkie klasyfikacje sa
+            self.classification = (1, m[0][-1] )
+            print self.classification
         elif len(m) == 2: #pozostaja dwa wiec ciezko rozroznic na podstawie indexu Giniego.
             i = 0
             while m[0][i] == m[1][i]:
@@ -26,7 +28,7 @@ class Node():
             self.value = m[0][i]
             self.left = Node(None, None, None,None )
             self.right = Node(None, None, None , None)
-            self.left.insert(m[0]), self.right.insert(m[1])
+            return self.left.insert(m[0]), self.right.insert(m[1])
         elif self.check_identity(m): #wszystkie cechy takie same ale rozna klasyfikacja
             counting = [x[-1] for x in m].count(m[0][-1]) / float(len(m))
             if counting > 0.5:
@@ -40,9 +42,13 @@ class Node():
             index_G = self.index_Giniego(m)
             self.value = index_G[2]
             self.nr_index = index_G[1]
-            left_branch = [x for x in m if x[index_G[1]] < index_G[2] ]
-            right_branch = [x for x in m if x[index_G[1]] >= index_G[2] ]
-            print m, '\n', left_branch, right_branch, '\n'
+            if types(index_G[1]):
+                left_branch = [x for x in m if x[index_G[1]] < index_G[2] ]
+                right_branch = [x for x in m if x[index_G[1]] >= index_G[2] ]
+            else:
+                left_branch = [x for x in m if x[index_G[1]] == index_G[2] ]
+                right_branch = [x for x in m if x[index_G[1]] != index_G[2] ]
+            #print m, '\n', left_branch, right_branch, '\n'
             self.nr_index = index_G[1]
             self.value = index_G[2]
             self.left = Node(None, None, None, None )
@@ -52,6 +58,7 @@ class Node():
             
 
     def check_last(self, m):
+        print 'm', m
         tmp = m[0][-1]
         for x in m:
             if x[-1] != tmp:
@@ -76,7 +83,7 @@ class Node():
         j = 0
         mini = (1,0,0)
         while j <= len(matrix[0]) -2:
-            if types[j] == 0:
+            if types[j]:
                 tmpl = [ [x[j], x[-1]] for x in matrix]
                 mini_tmp = self.index_Giniego_wektor_liczby(tmpl)
             else:
@@ -166,23 +173,21 @@ class Tree():
         self.root = Node (self, None, None, None)
 
     def insert (self, matrix):
-        global types # (0- int or float, 1- bool, 2-str)
+        global types # (True if int or float; False- str or bool)
         types = []
         for lines in matrix:
             for x in lines:
-                value = 1
-                if type(x) == str:
-                    value = 2
+                value = True
+                if type(x) == str or type(x) == bool:
+                    value = False
                     break
-                elif type(x) == float or type(x) == int:
-                    value == 0
             types.append(value)
         self.root.insert(matrix)
     
     def go_through(self, m):
         print self.root.go_through(m)
 
-test = [ [1 ,3, 4,True] , [1 ,3, 4,True], [ 1,2,3, False], [1,2,5, True] ]
+test = [[6.0, '12aaa', 54, True], [-1, '00000iiii',0, False]]
 
 d =Tree()
 d.insert(test)
